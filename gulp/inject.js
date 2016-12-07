6c3094f2d0efbfaee8dc5a64fcb5c143897f9011
Vicente Neto <sneto.vicente@gmail.com>
2016-12-06 23:08:07 -0300

    Creating gulp task to automatically inject dependencies

---
+++ gulp/inject.js
@@ -0,0 +1,35 @@
+'use strict';
+
+var path = require('path');
+var gulp = require('gulp');
+var conf = require('./conf');
+
+var $ = require('gulp-load-plugins')();
+
+var wiredep = require('wiredep').stream;
+var _ = require('lodash');
+
+gulp.task('inject', function () {
+  var injectStyles = gulp.src(
+    [path.join(conf.paths.app, '/**/*.css')],
+    {read: false}
+  );
+
+  var injectScripts = gulp.src([
+    path.join(conf.paths.app, '/app.js'),
+    path.join(conf.paths.app, '/**/*.js'),
+    path.join('!' + conf.paths.app, '/bower_components/**')
+  ])
+    .pipe($.angularFilesort()).on('error', conf.errorHandler('AngularFilesort'));
+
+  var injectOptions = {
+    ignorePath: [conf.paths.app, path.join(conf.paths.tmp, '/serve')],
+    addRootSlash: false
+  };
+
+  return gulp.src(path.join(conf.paths.app, '/*.html'))
+    .pipe($.inject(injectStyles, injectOptions))
+    .pipe($.inject(injectScripts, injectOptions))
+    .pipe(wiredep(_.extend({}, conf.wiredep)))
+    .pipe(gulp.dest(path.join(conf.paths.tmp, '/serve')));
+});
